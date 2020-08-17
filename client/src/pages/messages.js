85a5ef820c2fd3e358bc2aaff685b07749ea301b
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-08-17 02:44:44 -0700

    have the server and client able to send and recieve data from each other via socket.io, need to build a bridge between multiple apps

--- client/src/pages/messages.js
+++ client/src/pages/messages.js
@@ -1,17 +1,52 @@
-import React from 'react';
+import React, {useState} from 'react';
 import io from 'socket.io-client';
 
 
 function Messenger () {
-    const socket = io('http://localhost:3001/');
+    const [formState, setFormState] = useState({ message: ''})
 
+    // establish the url to the server side.
+    const socket = io('http://localhost:3001/');
+    // establish the connection
     socket.on('connect', () => {
-        console.log(socket.connected);
+        //check if connected
+        // console.log(socket);
     });
+    socket.on("message", ( data ) => {
+        console.log(data)
+    })
+
+    const SubmitHandler = async event => {
+        // prevent the page from refreshing
+        event.preventDefault();
+        // send the message to the server
+        socket.send(formState.message, "/")
+        // set message state back to empty
+        setFormState({ message: ''})
+    }
+
+    const handleChange = event => {
+        // deconstruct the event so it targets the "message" and the input of the message.
+        const { name, value } = event.target;
+        // set the form state to the new input so when the user hits submit it send the formState to the server.
+        setFormState({
+            ...formState,
+            [name]: value
+        })
+    }
 
     return (
         <div>
-            {!socket.connected ? (<h5>connected</h5>) : (<h5>not connected</h5>)} 
+            {!socket.connected ? (<h5>connected</h5>) : (<h5>not connected</h5>)}
+            <form onSubmit={SubmitHandler}>
+                <textarea
+                placeholder="Chat now!"
+                onChange={handleChange}
+                name="message"
+                value={formState.message}
+                ></textarea>
+                <button>Submit</button>    
+            </form> 
         </div>
     )
 }
