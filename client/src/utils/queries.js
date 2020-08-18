2f60dc0be87f44d1bd5059665928c287a742ed25
Drkeck <k3ckl3y.a@gmail.com>
2020-08-18 01:06:10 -0700

    added basic gql to the client side of the project, will be expanding on these when needed.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,18 @@
 import gql from 'graphql-tag';
+
+export const ME = gql`
+    Query Me {
+        User {
+            username
+            email
+        }
+    }
+`;
+
+export const User = gql`
+    Query user {
+        User {
+            username
+        }
+    }
+`;
