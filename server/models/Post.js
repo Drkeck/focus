a68bb1f47eff15636bb1765464f2fc12fc0f30fa
rawanfatima <rawanfatima@outlook.com>
2020-08-18 13:28:34 -0700

    updated post.js

--- server/models/Post.js
+++ server/models/Post.js
@@ -1,68 +1,28 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
 
-// create our Post model
-class Post extends Model {
-    static upvote(body, models) {
-        return models.Vote.create({
-            user_id: body.user_id,
-            post_id: body.post_id
-        })
-        .then(() => {
-            return Post.findOne({
-                where: {
-                    id: body.post_id
-                },
-                attributes: [
-                    'id',
-                    'post_url',
-                    'title',
-                    'created_at',
-                    [
-                        sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-                        'vote_count'
-                    ]
-                ]
-            });
-        });
-  
-    }
-};
+const postSchema = new Schema({
+  user: { 
+    type: Schema.Types.ObjectId, 
+    ref: 'User' 
+},
+content: { 
+    type: String, 
+    required: true, 
+    trim: true
+},
+image: String,
+title: {
+  type: String,
+  required: true
+},
+createdAt: {
+  type: Date,
+  default: Date.now,
+},
 
-// create fields/columns for Post model
-Post.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    post_url: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isURL: true
-      }
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      references: {
-        model: 'user',
-        key: 'id'
-      }
-    }
-  },
-  {
-    sequelize,
-    freezeTableName: true,
-    underscored: true,
-    modelName: 'post'
-  }
-);
+  timestamps: true
+});
+
+const Post = mongoose.model('Post', postSchema);
 
 module.exports = Post;
