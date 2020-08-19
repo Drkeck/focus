c99e655600da521a51aa7b85608fe016ea7108ef
alexander keckley <61635682+Drkeck@users.noreply.github.com>
2020-08-19 14:30:22 -0700

    fixed up friends list a little bit

--- client/src/components/friendsList/index.js
+++ client/src/components/friendsList/index.js
@@ -1,15 +1,13 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useQuery } from '@apollo/react-hooks';
 import { ME } from '../../utils/queries';
 
 function FriendsList() {
-    // const[friendsList, setFriendsList] = useState({friends:{}})
     const { data, loading } = useQuery(ME)
     const friends = data?.Me?.friends || [{}]
-    console.log(friends)
     return (
         <div>
-            {friends?.map((frnd, index) => (
+            {friends.map((frnd, index) => (
             <div key={index}>
                 <h5>{frnd.username}</h5>
                 <p>{frnd._id}</p>
