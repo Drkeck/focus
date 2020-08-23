f98d33179215aa0b779ed722d12025ff06aa9b8e
Drkeck <k3ckl3y.a@gmail.com>
2020-08-23 09:14:14 -0700

    attempting to push to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/focus', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/fo-cus', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
